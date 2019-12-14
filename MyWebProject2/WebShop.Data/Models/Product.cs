231e6adc6c49268266aa5481dae754681a46c0b3
youngvictormonet <victorstrelnikov91@gmail.com>
2019-12-14 23:48:56 +0300

    Product class initializing

---
+++ MyWebProject2/WebShop.Data/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShop.WebShop.Data.Models
+{
+    public class Product
+    {
+    }
+}
