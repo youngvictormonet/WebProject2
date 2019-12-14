a2b870a9c4bde05027ae365b538fafbba13a2532
youngvictormonet <victorstrelnikov91@gmail.com>
2019-12-15 00:48:10 +0300

    DB Initializing

---
+++ MyWebProject2/Data/DataBaseInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShop.Data
+{
+    public class DataBaseInitializer
+    {
+    }
+}
