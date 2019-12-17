ee742310229354bc820370ef074ba2abb4335897
youngvictormonet <victorstrelnikov91@gmail.com>
2019-12-17 10:45:43 +0300

    Contact VM adding

---
+++ MyWebProject2/WebShop/ViewModels/ContactUS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShop.ViewModels
+{
+    public class ContactUS
+    {
+        public string UserName { get; set; }
+
+
+        public string UserEmail { get; set; }
+
+        public string Message { get; set; }
+
+    }
+}
